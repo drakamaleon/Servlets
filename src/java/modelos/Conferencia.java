604d5bfa6c1b9aa08b865edb018c942d5eb64d34
Cliente CONTROL-PC <Cliente CONTROL-PC>
2017-01-11 09:51:45 -0500

    rudy es loca

--- src/java/modelos/Conferencia.java
+++ src/java/modelos/Conferencia.java
@@ -5,10 +5,43 @@
  */
 package modelos;
 
-/**
- *
- * @author CltControl
- */
+import java.util.Date;
+
+
 public class Conferencia {
+    String nombre;
+    String descripcion;
+    Date fecha;
+
+    public Conferencia(String nombre, String descripcion, Date fecha) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.fecha = fecha;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+    
     
 }

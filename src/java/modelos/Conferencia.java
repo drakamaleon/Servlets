b8c9308106db400e68165ea3a979566b3303415e
hjhj <jjjjjgh@hotmail.com>
2017-01-11 09:55:04 -0500

    modify

--- src/java/modelos/Conferencia.java
+++ src/java/modelos/Conferencia.java
@@ -9,14 +9,24 @@ import java.util.Date;
 
 
 public class Conferencia {
+    int id;
     String nombre;
     String descripcion;
     Date fecha;
-
-    public Conferencia(String nombre, String descripcion, Date fecha) {
+   
+    public Conferencia(int id,String nombre, String descripcion, Date fecha) {
+        this.id = id;
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.fecha = fecha;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getNombre() {
